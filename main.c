de8b6031766c7d3fdb3d1aac262458361b8617df
tharini <matharinithamalja2006.com>
2026-09-14 21:45:01 +0530

    Add hospital system welcome message

--- main.c
+++ main.c
@@ -3,6 +3,20 @@
 
 int main()
 {
-    printf("Smart Hospital & Resource Allocation System\n");
+    int specialtyId;
+    printf("Smart Hospital & Resource Allocation System\n\n");
+
+    printf("Doctor Specialties\n");
+    printf("1. General Practice(OPD) - Rs.1500.00 - 15 minutes\n");
+    printf("2. Paediatrics - Rs.2500.00 - 20 minutes\n");
+    printf("3. Cardiology - Rs.4500.00 - 30 minutes\n");
+    printf("4. Neurology - Rs.5000.00 - 30 minutes\n");
+
+    printf("\nEnter Specialty ID: ");
+    scanf("%d",&specialtyId);
+
+
+
+
     return 0;
 }
